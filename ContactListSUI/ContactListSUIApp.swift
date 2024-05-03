40160fdc5daa615ba7dd68062a90d17e1568bc62
popov <popich2005@mail.ru>
2024-05-03 12:48:49 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Алексей Попов on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
